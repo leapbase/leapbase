63eca068ec4096cc455a2f27b795e75e44e33998
leapbase <yliu0@leapbase.com>
2019-08-31 19:38:45 -0400

    clean up library links; cleanup setting-local.js

--- site/setting/setting-local.js
+++ site/setting/setting-local.js
@@ -2,19 +2,15 @@ exports.setting = {
   // web
   name: 'Leapbase',
   http_port: 8060,
-  // https_port: 8443,
   webserver_baseurl: 'http://localhost:8060',
   // invite code
   invite_code_user: 'hello101test',
   invite_code_admin: 'world101test',
   // user authentication
-  user_module: 'user', // user/member
-  // show_user_dropdown: false, // set to true if user module is loaded
+  user_module: 'user',
   ssl: {
     enable: false,
     http_redirect: false
-    // cert_file: './data/sslcert/fullchain.pem',
-    // key_file: './data/sslcert/privkey.pem'
   },
   // database
   database: {
